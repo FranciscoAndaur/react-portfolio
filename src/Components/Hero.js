30962cf5517663c4a02c915615b5f59b48e05ea4
Fraciscv <Francisco.Andaur@icloud.com>
2020-12-04 22:13:57 -0500

    added caraousel component, needs images

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => {
                     <Col md={8} sm={12}>
                     { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
                     { props.title && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
-                    { props.title && <h3 className="dlead font-weight-light">{props.text}</h3> }
+                    { props.title && <h3 className="lead font-weight-light">{props.text}</h3> }
                     </Col>
                 </Row>
             </Container>

442cd6a7318ba9ab3e94d0cf2c8a0ee58d75e739
Fraciscv <Francisco.Andaur@icloud.com>
2020-12-04 22:30:29 -0500

    changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
             <Container fluid={true}>
                 <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
-                    Francisco Andaur
+                    @hcaktehplaent
                     </Col>
                     <Col className="p-0 d-flex justify-content-end" md={3}>
-                    This site was made by Francisco Andaur.
+                    Made with ♥ by Francisco Andaur.
                     </Col>
                 </Row>
             </Container>

3370b4aae7ad91074886e9ba946493b20a194c2e
Fraciscv <Francisco.Andaur@icloud.com>
2020-12-12 18:55:32 -0500

    about page finished

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,11 +9,11 @@ const About = (props) => {
             <Hero title={props.title} />
 
                 <Content>
-                <p>Hello, my name is Francisco.  I'm a full-stack software engineer with experience in Ruby, Ruby on Rails, Javascript, and React. My dream is to live in a cabin in the mountains, but for now, I reside in the Bronx.</p> 
+                <p>Hello, my name is Francisco.  I'm a full-stack software engineer with experience in <strong>Ruby, Ruby on Rails, Javascript, and React</strong>. My dream is to live in a cabin in the mountains, but for now, I reside in the Bronx.</p> 
 
-                <p>After completing a six-month intensive code camp at Flatiron School I've realized that I am passionate about making human connections through technology. Currently learning and gaining more experience with React, Python, Javascript, CSS, and SQL.</p>
+                <p>After completing a six-month intensive code camp at Flatiron School I've realized that I am passionate about making human connections through technology. Currently learning and gaining more experience with <strong>React Native, Python, Javascript, CSS, and SQL</strong>.</p>
 
-                <p>My latest project, Dev Dream, is a dream log for people. You can check it out here, or on the homepage. It is built with React, Redux, and Grommet.io.  However, I will be rebuilding it as a mobile app using Redux in the following months.</p>
+                <p>My latest project, Dev Dream, is a dream log for people. You can check it out <a href="https://github.com/FranciscoAndaur/DevDream" target="_blank" rel="noopener noreferrer">here</a> , or on the homepage. It is built with <strong>React, Redux, and Grommet.io</strong>.  However, I will be rebuilding it as a mobile app using Redux in the following months.</p>
 
                 <p>When I'm not learning something new, I'm probably running or cycling around this wonderful city.</p>
                 </Content>
